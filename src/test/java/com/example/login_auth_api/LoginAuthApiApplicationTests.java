5da1d74fc43962145d3ecae22898a5ae6c89ae90
Arthur Bicalho <arthurbic95@gmail.com>
2025-04-12 09:36:46 -0300

    first commit

---
+++ src/test/java/com/example/login_auth_api/LoginAuthApiApplicationTests.java
@@ -0,0 +1,9 @@
+package com.example.login_auth_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAuthApiApplicationTests {
+
+}
